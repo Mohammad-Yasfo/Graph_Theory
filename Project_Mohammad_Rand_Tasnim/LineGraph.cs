df949867511859cc97d2f7e8485e97034571f425
unknown <mohammad.yasfo@gmail.com>
2018-01-13 19:46:11 +0200

    first commit

---
+++ Project_Mohammad_Rand_Tasnim/LineGraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphs_new
+{
+    public class LineGraph
+    {
+        public string name;
+        public NodeGraph begin;
+        public NodeGraph end;
+        public int w;  //Weight for The Dijkstra Algorithm
+
+        public bool visited;
+        public LineGraph()
+        {
+            this.visited = false;
+        }
+    }
+}
